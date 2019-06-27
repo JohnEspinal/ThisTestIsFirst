34c1b036d7456af7433beafaa478489a1432fbc4
John Marcos Espinal Hernandez <john-espinal10@hotmail.com>
2019-06-27 15:53:52 -0400

    This is the project

---
+++ HelloWorldWithGitHub/HelloWorldWithGitHub/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloWorldWithGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = { "I am ", "a daydreamer ", "and night as well" };
+
+            for (int i = 0; i <= array.Length-1; i++)
+            {
+                Console.Write(array[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
